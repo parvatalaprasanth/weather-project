8c39a7b7bd2ea9171e7b0a918792bad66374d377
parvatalaprasanth <49365892+parvatalaprasanth@users.noreply.github.com>
2021-01-22 18:12:05 +0530

    Add files via upload

---
+++ dbconfig.js
@@ -0,0 +1,14 @@
+const config={
+  user:"DESKTOP-TS7QTII\parva",
+  password:"",
+  server:"127.0.0.1",
+  database:"192.168.43.116",
+  options:{
+    trustedconnection:true,
+    "enableArithAbort": true,
+    instantName:"."
+  },
+  port: 1433
+}
+
+module.exports=config;
